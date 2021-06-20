9f2eedb5ebe38ebd82b3013a2259badb1e7f0344
T2019 <soilman@yandex.ru>
2021-06-21 00:39:05 +0300

    Инициализировал проект

---
+++ src/components/main-wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-wrapper">
+    <div class="container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main-wrapper'
+}
+</script>
+
+<style scoped>
+
+</style>
